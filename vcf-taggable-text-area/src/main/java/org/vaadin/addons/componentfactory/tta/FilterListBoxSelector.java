493156fb530058702d09199ff906e77fa1ab3974
Paola De Bartolo <paola.debartolo@gmail.com>
2025-01-09 11:54:03 -0300

    docs: update javadocs

--- vcf-taggable-text-area/src/main/java/org/vaadin/addons/componentfactory/tta/FilterListBoxSelector.java
+++ vcf-taggable-text-area/src/main/java/org/vaadin/addons/componentfactory/tta/FilterListBoxSelector.java
@@ -35,15 +35,34 @@ import com.vaadin.flow.data.selection.SingleSelect;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.function.SerializableBiFunction;
 
+/**
+ * A custom field component that combines a {@link TextField} with a {@link ListBox}.
+ * 
+ * <p>The {@code FilterListBoxSelector} allows to filter items in the list based on text input
+ * and select an item either by pressing Enter or interacting with the {@link ListBox}.
+ *
+ * @param <T> the type of items displayed in the {@link ListBox}
+ */
 @SuppressWarnings("serial")
 @CssImport("./styles/filter-listbox-selector.css")
 public class FilterListBoxSelector<T> extends CustomField<T> implements SingleSelect<CustomField<T>, T>, HasStyle {
 	
 	TextField filter = new TextField();
 	ListBox<T> listBox = new ListBox<>();
+	
+	/**
+     * Function defining the filter expression, used to match items against the filter query.
+     * Defaults to checking if the string representation of the item contains the filter text.
+     */
 	private SerializableBiFunction<T, String, Boolean> filterExpression = (item,filter) -> filter.contains(""+item);
+	
 	private List<T> filteredItems; 
 	
+    /**
+     * Constructs a {@code FilterListBoxSelector} with the given list of items.
+     *
+     * @param items the list of items to display in the {@link ListBox}
+     */
 	public FilterListBoxSelector(List<T> items) {
 	    setClassName("taggable-textarea-filter-listbox-selector");
 		listBox.setItems(items);
@@ -83,6 +102,11 @@ public class FilterListBoxSelector<T> extends CustomField<T> implements SingleSe
 		add(layout);
 	}
 	
+    /**
+     * Sets a custom renderer for displaying items in the {@link ListBox}.
+     *
+     * @param itemRenderer the renderer to use for items
+     */
     public void setRenderer(
             ComponentRenderer<? extends Component, T> itemRenderer) {
         this.listBox.setRenderer(itemRenderer);
@@ -103,10 +127,20 @@ public class FilterListBoxSelector<T> extends CustomField<T> implements SingleSe
 		listBox.setValue(newPresentationValue);
 	}
 
+    /**
+     * Returns the filter expression used to filter items.
+     *
+     * @return the filter expression
+     */
 	public SerializableBiFunction<T, String, Boolean> getFilterExpression() {
 		return filterExpression;
 	}
 
+    /**
+     * Sets the filter expression used to filter items.
+     *
+     * @param filterExpression the filter expression
+     */
 	public void setFilterExpression(SerializableBiFunction<T, String, Boolean> filterExpression) {
 		this.filterExpression = filterExpression;
 	}
